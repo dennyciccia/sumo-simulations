8bad6a08d4c8d3264ce266caba2c3ee08787f0a5
Denny <ciccia.denny@gmail.com>
2024-05-09 15:32:37 +0200

    eseguiti esperimenti manhattan3x3_100pc

--- utils/run_simulations.ps1
+++ utils/run_simulations.ps1
@@ -1,5 +1,5 @@
-python runner.py -p data\vehicle_population915.yaml -n 4way_crossing_160m -stl OFF
-python runner.py -p data\vehicle_population915.yaml -n 4way_crossing_160m -stl ON
-python runner.py -p data\vehicle_population915.yaml -n 4way_crossing_160m -stl ON -e 1
-python runner.py -p data\vehicle_population915.yaml -n 4way_crossing_160m -stl ON -e 2
-python runner.py -p data\vehicle_population915.yaml -n 4way_crossing_160m -stl ON -e 1 2
+python runner.py -p data\vehicle_population915.yaml -n manhattan3x3_100pc -stl OFF
+python runner.py -p data\vehicle_population915.yaml -n manhattan3x3_100pc -stl ON
+python runner.py -p data\vehicle_population915.yaml -n manhattan3x3_100pc -stl ON -e 1
+python runner.py -p data\vehicle_population915.yaml -n manhattan3x3_100pc -stl ON -e 2
+python runner.py -p data\vehicle_population915.yaml -n manhattan3x3_100pc -stl ON -e 1 2
