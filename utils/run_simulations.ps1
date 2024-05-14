fb78bc1a04fc1bf62e0a1d6d6ecee08b51a7ef30
Denny <ciccia.denny@gmail.com>
2024-05-14 17:12:50 +0200

    eseguiti esperimenti 4way_crossing_640m_junc

--- utils/run_simulations.ps1
+++ utils/run_simulations.ps1
@@ -1,5 +1,5 @@
-python runner.py -p data\vehicle_population915.yaml -n 4way_crossing_320m_junc -stl OFF
-python runner.py -p data\vehicle_population915.yaml -n 4way_crossing_320m_junc -stl ON
-python runner.py -p data\vehicle_population915.yaml -n 4way_crossing_320m_junc -stl ON -e 1
-python runner.py -p data\vehicle_population915.yaml -n 4way_crossing_320m_junc -stl ON -e 2
-python runner.py -p data\vehicle_population915.yaml -n 4way_crossing_320m_junc -stl ON -e 1 2
+python runner.py -p data\vehicle_population915.yaml -n 4way_crossing_640m_junc -stl OFF
+python runner.py -p data\vehicle_population915.yaml -n 4way_crossing_640m_junc -stl ON
+python runner.py -p data\vehicle_population915.yaml -n 4way_crossing_640m_junc -stl ON -e 1
+python runner.py -p data\vehicle_population915.yaml -n 4way_crossing_640m_junc -stl ON -e 2
+python runner.py -p data\vehicle_population915.yaml -n 4way_crossing_640m_junc -stl ON -e 1 2
