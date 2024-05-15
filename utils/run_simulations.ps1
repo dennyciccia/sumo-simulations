9f78061bf52a6785019ce043f2234774ce70f109
Denny <ciccia.denny@gmail.com>
2024-05-15 16:27:17 +0200

    eseguiti esperimenti manhattan7x7_100pc e generata relativa popolazione

--- utils/run_simulations.ps1
+++ utils/run_simulations.ps1
@@ -1,5 +1,5 @@
-python runner.py -p data\vehicle_population1707.yaml -n manhattan6x6_100pc -stl OFF
-python runner.py -p data\vehicle_population1707.yaml -n manhattan6x6_100pc -stl ON
-python runner.py -p data\vehicle_population1707.yaml -n manhattan6x6_100pc -stl ON -e 1
-python runner.py -p data\vehicle_population1707.yaml -n manhattan6x6_100pc -stl ON -e 2
-python runner.py -p data\vehicle_population1707.yaml -n manhattan6x6_100pc -stl ON -e 1 2
+python runner.py -p data\vehicle_population1971.yaml -n manhattan7x7_100pc -stl OFF
+python runner.py -p data\vehicle_population1971.yaml -n manhattan7x7_100pc -stl ON
+python runner.py -p data\vehicle_population1971.yaml -n manhattan7x7_100pc -stl ON -e 1
+python runner.py -p data\vehicle_population1971.yaml -n manhattan7x7_100pc -stl ON -e 2
+python runner.py -p data\vehicle_population1971.yaml -n manhattan7x7_100pc -stl ON -e 1 2
