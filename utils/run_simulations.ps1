b77c90d2bf99995de827ae68aaad0bf179094587
dennyciccia <ciccia.denny@gmail.com>
2024-05-21 16:22:32 +0200

    eseguiti esperimenti

--- utils/run_simulations.ps1
+++ utils/run_simulations.ps1
@@ -1,5 +1,11 @@
-python runner.py -p data\vehicle_population915.yaml -n 4way_crossing_320m_junc300m -stl OFF
-python runner.py -p data\vehicle_population915.yaml -n 4way_crossing_320m_junc300m -stl ON
-python runner.py -p data\vehicle_population915.yaml -n 4way_crossing_320m_junc300m -stl ON -e 1
-python runner.py -p data\vehicle_population915.yaml -n 4way_crossing_320m_junc300m -stl ON -e 2
-python runner.py -p data\vehicle_population915.yaml -n 4way_crossing_320m_junc300m -stl ON -e 1 2
+python runner.py -p data\vehicle_population915.yaml -n 4way_crossing_640m_junc100m -stl OFF
+python runner.py -p data\vehicle_population915.yaml -n 4way_crossing_640m_junc100m -stl ON
+python runner.py -p data\vehicle_population915.yaml -n 4way_crossing_640m_junc100m -stl ON -e 1
+python runner.py -p data\vehicle_population915.yaml -n 4way_crossing_640m_junc100m -stl ON -e 2
+python runner.py -p data\vehicle_population915.yaml -n 4way_crossing_640m_junc100m -stl ON -e 1 2
+
+python runner.py -p data\vehicle_population915.yaml -n 4way_crossing_1280m_junc100m -stl OFF
+python runner.py -p data\vehicle_population915.yaml -n 4way_crossing_1280m_junc100m -stl ON
+python runner.py -p data\vehicle_population915.yaml -n 4way_crossing_1280m_junc100m -stl ON -e 1
+python runner.py -p data\vehicle_population915.yaml -n 4way_crossing_1280m_junc100m -stl ON -e 2
+python runner.py -p data\vehicle_population915.yaml -n 4way_crossing_1280m_junc100m -stl ON -e 1 2
