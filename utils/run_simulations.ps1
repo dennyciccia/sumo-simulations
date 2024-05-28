2f2a6ed8cc704166338b3cfa2fdc075ccc228883
dennyciccia <ciccia.denny@gmail.com>
2024-05-28 10:51:09 +0200

    eseguiti esperimenti

--- utils/run_simulations.ps1
+++
@@ -1,5 +0,0 @@
-python runner.py -p data\vehicle_population17880.yaml -n manhattan8x8_100pc -stl OFF
-python runner.py -p data\vehicle_population17880.yaml -n manhattan8x8_100pc -stl ON
-python runner.py -p data\vehicle_population17880.yaml -n manhattan8x8_100pc -stl ON -e 2
-python runner.py -p data\vehicle_population17880.yaml -n manhattan8x8_100pc -stl ON -e 1
-python runner.py -p data\vehicle_population17880.yaml -n manhattan8x8_100pc -stl ON -e 1 2
