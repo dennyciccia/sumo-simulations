2ed59452a12fe1c07c6490887f021d3175d3fdbd
dennyciccia <ciccia.denny@gmail.com>
2024-07-17 08:54:14 +0200

    fix

--- utils/get_nvehicles_Bologna.py
+++ utils/get_nvehicles_Bologna.py
@@ -1,4 +1,7 @@
-# script che serve per estrarre i dati sul numero di veicoli circolanti a Bologna dal file del dataset
+# Script che serve per estrarre i dati sul numero di veicoli circolanti a Bologna dal file del dataset
+# Trova il totale del numero medio di veicoli che passano in tutte le vie tra le 18 e le 19.
+# Calcola per ogni via la media dei veicoli che ci passano durante l'anno tra le 18 e le 19.
+# Poi fa la somma dei numeri medi.
 
 import argparse
 import pandas as pd
@@ -8,32 +11,29 @@ parser.add_argument('-f', '--file-name', dest="filename", required=True, metavar
 parser.add_argument('-H', '--hour', dest="hour", required=True, metavar="HH:00-HH:00", help='Fascia oraria da cui estrarre i dati')
 arguments = parser.parse_args()
 
-# Totale del numero medio di veicoli che passano in tutte le vie tra le 18 e le 19.
-# Per ogni via la media dei veicoli che ci passano durante l'anno tra le 18 e le 19.
-# Somma dei numeri medi.
-
 df = pd.read_csv(arguments.filename, header=0, sep=';')
 totale_veicoli_nella_via = dict()
 totale_giorni_per_via = dict()
 media_veicoli_nella_via = dict()
 
+# ogni riga è un giorno dell'anno
 for _, row in df.iterrows():
+    # se la via si vede per la prima volta inizializza a zero il valore del dizionario
     if row["Nome via"] not in totale_veicoli_nella_via and row["Nome via"] not in totale_giorni_per_via:
         totale_veicoli_nella_via[row["Nome via"]] = 0
         totale_giorni_per_via[row["Nome via"]] = 0
 
+    # aggiungi i veicoli che passano in quella via in quel giorno dell'anno
     totale_veicoli_nella_via[row["Nome via"]] += row[arguments.hour]
     totale_giorni_per_via[row["Nome via"]] += 1
 
+# media dei veicoli che passano nella via durante l'anno
 for key, value in totale_veicoli_nella_via.items():
     media_veicoli_nella_via[key] = value / totale_giorni_per_via[key]
 
+# somma del numero medio di veicoli che passano nelle vie durante l'anno
 somma = 0
 for value in media_veicoli_nella_via.values():
     somma += value
 
-print(totale_veicoli_nella_via["VIA RAVENNA"])
-print(totale_giorni_per_via["VIA RAVENNA"])
-print(media_veicoli_nella_via["VIA RAVENNA"])
-print(len(totale_veicoli_nella_via))
-print("somma: ", somma)
+print("Totale veicoli: ", somma)
