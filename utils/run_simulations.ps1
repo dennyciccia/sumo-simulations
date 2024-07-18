2d47e02fc9e8789cd2db8183b709e95e81f76440
dennyciccia <ciccia.denny@gmail.com>
2024-07-18 09:49:32 +0200

    aggiunte popolazioni per bologna

--- utils/run_simulations.ps1
+++ utils/run_simulations.ps1
@@ -1,2 +1,3 @@
-python runner.py -p data\vehicle_population4470.yaml -n manhattan8x8_100pc -stl ON
-python runner.py -p data\vehicle_population4470.yaml -n manhattan8x8_100pc -stl ON -e 3
+python runner.py -p .\data\vehicle_population18000.yaml -n bologna -stl ON
+python runner.py -p .\data\vehicle_population18000.yaml -n bologna -stl OFF
+python runner.py -p .\data\vehicle_population18000.yaml -n bologna -stl ON -e 3
