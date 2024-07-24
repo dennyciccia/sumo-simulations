589410c2e095c8d8e93d38e359e401fe3ae20fac
dennyciccia <ciccia.denny@gmail.com>
2024-07-24 14:59:19 +0200

    eseguiti esperimenti MASA

--- utils/run_simulations.ps1
+++ utils/run_simulations.ps1
@@ -1,3 +1,11 @@
-python runner.py -p data\vehicle_population2500.yaml -n masa -stl OFF
-python runner.py -p data\vehicle_population2500.yaml -n masa -stl ON
-python runner.py -p data\vehicle_population2500.yaml -n masa -stl ON -e 3
+python runner.py -p data\vehicle_population2500.yaml -n masa_25pc -stl OFF
+python runner.py -p data\vehicle_population2500.yaml -n masa_25pc -stl ON
+python runner.py -p data\vehicle_population2500.yaml -n masa_25pc -stl ON -e 3
+
+python runner.py -p data\vehicle_population2500.yaml -n masa_50pc -stl OFF
+python runner.py -p data\vehicle_population2500.yaml -n masa_50pc -stl ON
+python runner.py -p data\vehicle_population2500.yaml -n masa_50pc -stl ON -e 3
+
+python runner.py -p data\vehicle_population2500.yaml -n masa_75pc -stl OFF
+python runner.py -p data\vehicle_population2500.yaml -n masa_75pc -stl ON
+python runner.py -p data\vehicle_population2500.yaml -n masa_75pc -stl ON -e 3
